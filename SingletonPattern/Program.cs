6efe03e7df830d8b186f9f5448daba086b9a7772
mark.chua03@gmail.com <mark.chua03@gmail.com>
2016-04-08 11:25:28 +0800

    from dofactory

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DoFactory.GangOfFour.Singleton.Structural
+{
+    /// <summary>
+    /// MainApp startup class for Structural
+    /// Singleton Design Pattern.
+    /// </summary>
+    class MainApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            // Constructor is protected -- cannot use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            // Test for same instance
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    class Singleton
+    {
+        private static Singleton _instance;
+        
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
